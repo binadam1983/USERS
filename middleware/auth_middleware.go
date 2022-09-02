d3cef7d30994cd5d04bea7c8c712ce557ed8c426
BinAdam1983 <messagenaeem@skiff.com>
2022-09-03 00:16:29 +0500

    added updated jwt library

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -15,12 +15,12 @@ func AuthMiddleware() gin.HandlerFunc {
 		if err != nil || user == "" {
 			c.String(http.StatusUnauthorized, err.Error())
 			log.Info(user, "User not logged in, please login first")
-			//calling "Abort" to make sure success handlers are not called
+			//calling "Abort" to make sure successive handlers are not called
 			c.Abort()
 			return
 		}
-		log.Info("User Valid > ", user)
-		// allowing for the success handlers to be called
+		log.Info("User Valid ", user)
+		// allowing for the successive handlers to be called
 		c.Next()
 	}
 
